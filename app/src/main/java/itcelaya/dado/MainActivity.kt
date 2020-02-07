d1436ef41c3ba8fec2f12b9fd59d185632626623
isctorres <isctorres@gmail.com>
2020-02-06 22:54:14 -0600

    creacion del repositorio

---
+++ app/src/main/java/itcelaya/dado/MainActivity.kt
@@ -0,0 +1,32 @@
+package itcelaya.dado
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    public fun rollDice(view: View){
+
+        val randomInt = Random.nextInt(6)+1
+        val drawableResources = when(randomInt){
+            1->R.drawable.dice_1
+            2->R.drawable.dice_2
+            3->R.drawable.dice_3
+            4->R.drawable.dice_4
+            5->R.drawable.dice_5
+            else->R.drawable.dice_6
+        }
+
+        val diceImagen: ImageView = dice_image
+        diceImagen.setImageResource(drawableResources)
+    }
+}
